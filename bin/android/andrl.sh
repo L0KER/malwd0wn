ae2737a6623f362603028436ec0a2951bd79a12a
L0KER <44724465+L0KER@users.noreply.github.com>
2019-06-02 18:30:57 +0600

    Fixed files

--- bin/android/andrl.sh
+++ bin/android/andrl.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-curl -LO https://raw.githubusercontent.com/L0KER/malwbin/master/android/AndroidLocker.zip
-curl -LO https://raw.githubusercontent.com/L0KER/malwbin/master/android/AndroidLocker.pass
+curl -LO https://raw.githubusercontent.com/L0KER/malwbin/master/android/AndroidRansomware.zip
+curl -LO https://raw.githubusercontent.com/L0KER/malwbin/master/android/AndroidRansomware.pass
 echo """
 #---------------#
 |    malwd0wn   |     
-| AndroidLocker |
+|   Ransomware  |
 #---------------#
 """
 echo "[*] Downloading complete!"
