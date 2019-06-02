4713024e8ff2644c6f8e6f0f32736d26c3faee05
L0KER <44724465+L0KER@users.noreply.github.com>
2019-06-02 16:18:44 +0600

    Update admin.sh

--- bin/android/admin.sh
+++ bin/android/admin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -LO https://raw.githubusercontent.com/L0KER/malwbin/android/AndroidAdministrator.zip
-curl -LO https://raw.githubusercontent.com/L0KER/malwbin/android/AndroidAdministrator.pass
+curl -LO https://raw.githubusercontent.com/L0KER/malwbin/master/android/AndroidAdministrator.zip
+curl -LO https://raw.githubusercontent.com/L0KER/malwbin/master/android/AndroidAdministrator.pass
 echo """
 #---------------#
 |    malwd0wn   |     
