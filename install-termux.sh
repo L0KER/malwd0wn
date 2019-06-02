b8f4c2834ab5a94a7249873d2ed286cfa04c7500
L0KER <44724465+L0KER@users.noreply.github.com>
2019-06-02 17:14:15 +0600

    Update install-termux.sh

--- install-termux.sh
+++ install-termux.sh
@@ -5,6 +5,8 @@ echo """
 | malwd0wn termux installer |
 #---------------------------#
 """
+cd bin && cd android && chmod +x * && cd ..
+cd windows && chmod +x * && cd .. && cd ..
 chmod +x malwd0wn.py
 pkg install curl
 pkg install python2
