b6136baf6784e8a5d93dc0e3177392a4e26b857a
L0KER <44724465+L0KER@users.noreply.github.com>
2019-07-06 20:01:31 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,11 @@ echo """
 """
 cd bin && cd android && chmod +x * && cd ..
 cd windows && chmod +x * && cd ..
-cd macos && chmod +x * && cd .. && cd ..
+cd macos && chmod +x * && cd ..
+cd linux && chmod +x * && cd .. && cd ..
 chmod +x malwd0wn.py
-apt-get install curl
-apt-get install python2
+apt-get install curl -y
+apt-get install python2 -y
 echo """
 #------malwd0wn-------#
 | malwd0wn installed! |
